f0bfb995db9973781edf2d24d1db8e3f9cfb5ba0
Keanin Cupido <keanin16@gmail.com>
2024-04-06 21:15:18 +0200

    add news components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
-export const App = () => {
+import Navbar from "./components/Navbar";
+
+const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Navbar />
+    </div>
   )
 }
+
+export default App;
+
+// NewsAPI API key: d1688358175d473c81c48b9d22a73075
